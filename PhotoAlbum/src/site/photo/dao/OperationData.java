2f84190af2ee80da4539232759584c0ba3706119
yueyiqiu <yueyiqiu100@gmail.com>
2015-03-12 21:27:10 +0800

    201503122126

--- PhotoAlbum/src/site/photo/dao/OperationData.java
+++ PhotoAlbum/src/site/photo/dao/OperationData.java
@@ -1,5 +1,17 @@
 package site.photo.dao;
 
+import java.util.*;
+
+import site.photo.tool.JDBConnection;
+
 public class OperationData {
-		
+	private JDBConnection jdbc=new JDBConnection();
+	private List list = null;
+	private String sql = null;
+	
+	public List queryPhotoList(){
+		list=new ArrayList();
+		return null;
+	}
+	
 }
---
+++ PhotoAlbum/src/site/photo/model/Photo.java
@@ -0,0 +1,76 @@
+package site.photo.model;
+
+public class Photo {
+	private int id=0;
+	private String photoName="";
+	private String photoSize="";
+	private String photoType="";
+	private String photoTime="";
+	private String photoAddress="";
+	private String username="";
+	private String printAddress="";
+	private String smallPhoto="";
+	private int number=0;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPhotoName() {
+		return photoName;
+	}
+	public void setPhotoName(String photoName) {
+		this.photoName = photoName;
+	}
+	public String getPhotoSize() {
+		return photoSize;
+	}
+	public void setPhotoSize(String photoSize) {
+		this.photoSize = photoSize;
+	}
+	public String getPhotoType() {
+		return photoType;
+	}
+	public void setPhotoType(String photoType) {
+		this.photoType = photoType;
+	}
+	public String getPhotoTime() {
+		return photoTime;
+	}
+	public void setPhotoTime(String photoTime) {
+		this.photoTime = photoTime;
+	}
+	public String getPhotoAddress() {
+		return photoAddress;
+	}
+	public void setPhotoAddress(String photoAddress) {
+		this.photoAddress = photoAddress;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPrintAddress() {
+		return printAddress;
+	}
+	public void setPrintAddress(String printAddress) {
+		this.printAddress = printAddress;
+	}
+	public String getSmallPhoto() {
+		return smallPhoto;
+	}
+	public void setSmallPhoto(String smallPhoto) {
+		this.smallPhoto = smallPhoto;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
