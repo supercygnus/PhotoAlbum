2bac13bc93de8ef5388d2a09051291c3062cb283
supercygnus <yueyiqiu100@gmail.com>
2015-03-12 18:09:44 +0800

    201503121809

--- PhotoAlbum/src/site/photo/tool/JDBConnection.java
+++ PhotoAlbum/src/site/photo/tool/JDBConnection.java
@@ -1,5 +1,70 @@
 package site.photo.tool;
 
+import java.sql.*;
+
+
 public class JDBConnection {
+	private final String dbDriver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private final String url="jdbc:sqlserver://localhost;integratedSecurity=true;DatabaseName=db_photo";
+	private final String username="";
+	private final String password="";
+	private Connection conn = null;
 	
+	public JDBConnection(){
+		try{
+			Class.forName(dbDriver).newInstance();
+		}
+		catch(Exception e){
+			System.err.println("連接失敗");
+		}
+	}
+	
+	public boolean createConnection(){
+		try{
+			conn=DriverManager.getConnection(url, username, password);
+			return true;
+		}
+		catch(Exception e){
+			System.err.println("取得連線失敗");
+			return false;
+		}
+	}
+	
+	public boolean executeUpdate(String sql){
+		
+		if(conn==null)
+			this.createConnection();
+		
+		try{
+			Statement stmt=conn.createStatement();
+			int count=stmt.executeUpdate(sql);
+			System.out.println("執行成功,影響的記錄數為:"+count);
+			return true;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+	public ResultSet executeQuery(String sql){
+		
+		ResultSet rs=null;
+		if(conn==null)
+			this.createConnection();
+		try{
+			Statement stmt=conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			rs=stmt.executeQuery(sql);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+		return rs;
+	}
+	
+	public static void main(String[] args){
+		JDBConnection jc=new JDBConnection();
+		System.out.println(jc.createConnection());
+	}
 }
